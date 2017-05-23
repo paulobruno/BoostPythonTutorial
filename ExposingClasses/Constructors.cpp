fbc674d002cb5f976d860dd3e736a6b832115f3d
Paulo Bruno <paulobruno@alu.ufc.br>
2017-05-23 14:56:21 -0300

    Add "Constructors" part of ExposingClasses project
    This is the second part of the ExposingClasses "chapter".

---
+++ ExposingClasses/Constructors.cpp
@@ -0,0 +1,22 @@
+#include <Python.h>
+#include <boost/python.hpp>
+
+
+struct World
+{
+    World(std::string msg): msg(msg) {}
+    void set(std::string msg) { this->msg = msg; }
+    std::string greet() { return msg; }
+    std::string msg;
+};
+
+
+BOOST_PYTHON_MODULE(ExposingClasses)
+{
+    using namespace boost::python;
+    
+    class_<World>("World", init<std::string>())
+        .def("greet", &World::greet)
+        .def("set", &World::set)
+    ;
+}
