1bb0481ee63535c4e8e8b69b704d5fcbebfd7987
paulobruno <paulobruno@alu.ufc.br>
2017-05-26 15:26:25 -0300

    Add NoConstructor project
    In this project we see how to not expose any constructor. We can use this
    in abstract classes.

---
+++ ExposingClasses/NoConstructor.cpp
@@ -0,0 +1,25 @@
+//http://www.boost.org/doc/libs/1_64_0/libs/python/doc/html/tutorial/tutorial/exposing.html#tutorial.exposing.constructors
+
+
+#include <Python.h>
+#include <boost/python.hpp>
+
+
+struct World
+{
+    World(std::string msg): msg(msg) {}
+    void set(std::string msg) { this->msg = msg; }
+    std::string greet() { return msg; }
+    std::string msg;
+};
+
+
+BOOST_PYTHON_MODULE(NoConstructor)
+{
+    using namespace boost::python;
+    
+    class_<World>("World", no_init)
+        .def("greet", &World::greet)
+        .def("set", &World::set)
+    ;
+}
